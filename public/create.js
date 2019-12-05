f63d57be49d105f0e58038618d5fd8b1dc6a0e34
Dani Foldi <dani@stefan.hu>
2019-12-05 22:07:09 +0100

    Improved create poll page

--- public/create.js
+++ public/create.js
@@ -1,8 +1,5 @@
 addEventListener('load', async () => {
-  document.querySelector('form').addEventListener('submit', async event => {
-    event.preventDefault()
-  })
-  document.querySelector('input[type=submit]').addEventListener('click', async event => {
+  document.querySelector('button').addEventListener('click', async event => {
     const response = await fetch('/poll', {
       method: 'POST',
       headers: {
@@ -10,31 +7,33 @@ addEventListener('load', async () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        question: document.querySelector('#question-field').value,
-        options: Array.from(document.querySelectorAll('.answer-field')).map(el => el.value).filter(el => el !== '')
+        question: document.querySelector('#question').value,
+        options: Array.from(document.querySelectorAll('.answer')).map(el => el.value).filter(el => el !== '')
       })
     })
     const body = await response.json()
     if (body.error) {
       alert('An error occured, please try again.')
     } else {
-      const element = document.querySelector('input[type="submit"]')
+      const button = document.querySelector('button')
+      const element = document.createElement('input')
       element.type = 'text'
+      element.readonly = true
       element.value = location.protocol + '//' + location.host + '/poll/' + body.url
+      button.insertAdjacentElement('afterend', element)
       element.select()
       element.setSelectionRange(0, 99999)
-      console.log(document.execCommand('copy'))
-      console.log(body.url)
+      button.parentNode.removeChild(button)
     }
   })
 
   async function addNew() {
-    if (Array.from(document.querySelectorAll('.answer-field')).map(el => el.value).filter(el => el === '').length === 0) {
-      const clone = document.querySelector('.answer-field').cloneNode()
+    if (Array.from(document.querySelectorAll('.answer')).map(el => el.value).filter(el => el === '').length === 0) {
+      const clone = document.querySelector('.answer').cloneNode()
       clone.value = ''
       clone.addEventListener('input', addNew)
-      document.querySelector('input[type="submit"]').insertAdjacentElement('beforebegin', clone)
+      document.querySelector('button').insertAdjacentElement('beforebegin', clone)
     }
   }
-  document.querySelectorAll('.answer-field').forEach(el => el.addEventListener('input', addNew))
+  document.querySelectorAll('.answer').forEach(el => el.addEventListener('input', addNew))
 })
